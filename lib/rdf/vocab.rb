18b9dbd6a331a5826b25cf6065cc254aa56f3131
Gregg Kellogg <gregg@greggkellogg.net>
2015-04-04 18:20:08 -0700

    Add vocabulary aliases for most vocabularies in RDF.rb.

--- lib/rdf/vocab.rb
+++ lib/rdf/vocab.rb
@@ -74,6 +74,40 @@ module RDF
       v = params.fetch(:class_name, id.to_s.upcase).to_sym
       autoload v, "rdf/vocab/#{id}"
     end
-    PROV = ::RDF::PROV if RDF.const_defined?(:PROV)
+
+    # Aliases for vocabularies still defined directly in RDF.rb
+    CC     = ::RDF::CC      if RDF.const_defined?(:CC)
+    CERT   = ::RDF::CERT    if RDF.const_defined?(:CERT)
+    DC     = ::RDF::DC      if RDF.const_defined?(:DC)
+    DC11   = ::RDF::DC11    if RDF.const_defined?(:DC11)
+    DCAT   = ::RDF::DCAT    if RDF.const_defined?(:DCAT)
+    DOAP   = ::RDF::DOAP    if RDF.const_defined?(:DOAP)
+    EXIF   = ::RDF::EXIF    if RDF.const_defined?(:EXIF)
+    FOAF   = ::RDF::FOAF    if RDF.const_defined?(:FOAF)
+    GEO    = ::RDF::GEO     if RDF.const_defined?(:GEO)
+    GR     = ::RDF::GR      if RDF.const_defined?(:GR)
+    HT     = ::RDF::HT      if RDF.const_defined?(:HT)
+    ICAL   = ::RDF::ICAL    if RDF.const_defined?(:ICAL)
+    MA     = ::RDF::MA      if RDF.const_defined?(:MA)
+    MO     = ::RDF::MO      if RDF.const_defined?(:MO)
+    OG     = ::RDF::OG      if RDF.const_defined?(:OG)
+    OGC    = ::RDF::OGC     if RDF.const_defined?(:OGC)
+    OWL    = ::RDF::OWL     if RDF.const_defined?(:OWL)
+    PROV   = ::RDF::PROV    if RDF.const_defined?(:PROV)
+    RSA    = ::RDF::RSA     if RDF.const_defined?(:RSA)
+    RSS    = ::RDF::RSS     if RDF.const_defined?(:RSS)
+    SCHEMA = ::RDF::SCHEMA  if RDF.const_defined?(:SCHEMA)
+    SIOC   = ::RDF::SIOC    if RDF.const_defined?(:SIOC)
+    SKOS   = ::RDF::SKOS    if RDF.const_defined?(:SKOS)
+    SKOSXL = ::RDF::SKOSXL  if RDF.const_defined?(:SKOSXL)
+    V      = ::RDF::V       if RDF.const_defined?(:V)
+    VCARD  = ::RDF::VCARD   if RDF.const_defined?(:VCARD)
+    VMD    = ::RDF::VMD     if RDF.const_defined?(:VMD)
+    VOID   = ::RDF::VOID    if RDF.const_defined?(:VOID)
+    VS     = ::RDF::VS      if RDF.const_defined?(:VS)
+    WRDS   = ::RDF::WRDS    if RDF.const_defined?(:WRDS)
+    WOT    = ::RDF::WOT     if RDF.const_defined?(:WOT)
+    XHTML  = ::RDF::XHTML   if RDF.const_defined?(:XHTML)
+    XML    = ::RDF::XML     if RDF.const_defined?(:XML)
   end
 end
