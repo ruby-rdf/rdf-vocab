39537a45881759d2b3136807a91c0d816f96044d
dchandekstark <dchandekstark@gmail.com>
2015-05-08 14:07:28 -0400

    Adds JSON-LD vocabulary

--- README.md
+++ README.md
@@ -27,6 +27,7 @@ Common OWL/RDFS Vocabularies for use with Ruby [RDF.rb][]
 * RDF::Vocab::ICAL      - [iCalendar vocabulary](http://www.w3.org/2002/12/cal/) (W3C) - Alias of `RDF::ICAL`
 * RDF::Vocab::Identifiers - [Standard Identifiers Scheme](http://id.loc.gov/vocabulary/identifiers.html) (LoC)
 * RDF::Vocab::IIIF      - [International Image Interoperability Framework Presentation API](http://iiif.io/api/presentation/2.0/) (IIIF)
+* RDF::Vocab::JSONLD    - [JSON-LD Vocabulary](http://www.w3.org/ns/json-ld)
 * RDF::Vocab::LDP       - [Linked Data Platform](http://www.w3.org/TR/ldp/) (W3C)
 * RDF::Vocab::MA        - [Ontology for Media Resources](http://www.w3.org/TR/mediaont-10/) (W3C) - Alias of `RDF::MA`
 * RDF::Vocab::MADS      - [Metadata Authority Description Schema](http://www.loc.gov/standards/mads/) (LoC)
---
+++ lib/rdf/vocab/jsonld.rb
@@ -0,0 +1,37 @@
+# -*- encoding: utf-8 -*-
+# This file generated automatically using vocab-fetch from http://www.w3.org/ns/json-ld#
+require 'rdf'
+module RDF::Vocab
+  class JSONLD < RDF::StrictVocabulary("http://www.w3.org/ns/json-ld#")
+
+    # Property definitions
+    property :context,
+      comment: %(This link relation is used to reference a JSON-LD context from a JSON document so that it can be interpreted as JSON-LD.).freeze,
+      label: "JSON-LD context".freeze,
+      "rdfs:isDefinedBy" => %(http://www.w3.org/ns/json-ld).freeze,
+      "rdfs:seeAlso" => %(http://www.w3.org/TR/json-ld-syntax/#referencing-contexts-from-json-documents).freeze,
+      type: "rdf:Property".freeze
+
+    # Extra definitions
+    term :compacted,
+      comment: %(This profile IRI is used to request or specify compacted JSON-LD document form.).freeze,
+      label: "Compacted JSON-LD document form".freeze,
+      "rdfs:isDefinedBy" => %(http://www.w3.org/ns/json-ld).freeze,
+      "rdfs:seeAlso" => %(http://www.w3.org/TR/json-ld-syntax/#iana-considerations).freeze
+    term :"compacted-flattened",
+      comment: %(This profile IRI is used to request or specify compacted, flattened JSON-LD document form.).freeze,
+      label: "Compacted, flattened JSON-LD document form".freeze,
+      "rdfs:isDefinedBy" => %(http://www.w3.org/ns/json-ld).freeze,
+      "rdfs:seeAlso" => %(http://www.w3.org/TR/json-ld-syntax/#iana-considerations).freeze
+    term :expanded,
+      comment: %(This profile IRI is used to request or specify expanded JSON-LD document form.).freeze,
+      label: "Expanded JSON-LD document form".freeze,
+      "rdfs:isDefinedBy" => %(http://www.w3.org/ns/json-ld).freeze,
+      "rdfs:seeAlso" => %(http://www.w3.org/TR/json-ld-syntax/#iana-considerations).freeze
+    term :"expanded-flattened",
+      comment: %(This profile IRI is used to request or specify expanded, flattened JSON-LD document form.).freeze,
+      label: "Expanded, flattened JSON-LD document form".freeze,
+      "rdfs:isDefinedBy" => %(http://www.w3.org/ns/json-ld).freeze,
+      "rdfs:seeAlso" => %(http://www.w3.org/TR/json-ld-syntax/#iana-considerations).freeze
+  end
+end
