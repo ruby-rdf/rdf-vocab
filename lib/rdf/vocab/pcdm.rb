dc79c18fb2585a7aa10a7cd78cc8edce0de13774
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2020-01-15 14:19:48 -0700

    Fix PCDM naming error

--- lib/rdf/vocab/pcdm.rb
+++ lib/rdf/vocab/pcdm.rb
@@ -5,9 +5,9 @@ require 'rdf'
 module RDF::Vocab
   # @!parse
   #   # Vocabulary for <http://pcdm.org/models#>
-  #   class PCDMModels < RDF::StrictVocabulary
+  #   class PCDM < RDF::StrictVocabulary
   #   end
-  class PCDMModels < RDF::StrictVocabulary("http://pcdm.org/models#")
+  class PCDM < RDF::StrictVocabulary("http://pcdm.org/models#")
 
     # Ontology definition
     ontology :"http://pcdm.org/models#",
@@ -26,9 +26,9 @@ module RDF::Vocab
         parent's members, and otherwise has all of the features of ordering \(some members may be
         omitted from the order, members may appear more than once in the order, etc.\).
       ).freeze,
-      isDefinedBy: "pcdmmodels:".freeze,
+      isDefinedBy: "pcdm:".freeze,
       label: "Alternate Order".freeze,
-      subClassOf: "pcdmmodels:Object".freeze,
+      subClassOf: "pcdm:Object".freeze,
       type: "rdfs:Class".freeze
     term :Collection,
       comment: %(
@@ -36,7 +36,7 @@ module RDF::Vocab
         and may links to works and/or collections. By default, member works and collections are an
         unordered set, but can be ordered using the ORE Proxy class.
       ).freeze,
-      isDefinedBy: "pcdmmodels:".freeze,
+      isDefinedBy: "pcdm:".freeze,
       label: "Collection".freeze,
       subClassOf: "ore:Aggregation".freeze,
       type: "rdfs:Class".freeze
@@ -47,7 +47,7 @@ module RDF::Vocab
         modification date, etc.\), but can also include properties related to preservation,
         digitization process, provenance, etc. Files MUST be contained by exactly one Object.
       ).freeze,
-      isDefinedBy: "pcdmmodels:".freeze,
+      isDefinedBy: "pcdm:".freeze,
       label: "File".freeze,
       type: "rdfs:Class".freeze
     term :Object,
@@ -58,7 +58,7 @@ module RDF::Vocab
         and is capable of standing on its own, being linked to from Collections and other Objects.
         Member Objects can be ordered using the ORE Proxy class.
       ).freeze,
-      isDefinedBy: "pcdmmodels:".freeze,
+      isDefinedBy: "pcdm:".freeze,
       label: "Object".freeze,
       subClassOf: "ore:Aggregation".freeze,
       type: "rdfs:Class".freeze
@@ -66,19 +66,19 @@ module RDF::Vocab
     # Property definitions
     property :fileOf,
       comment: %(Links from a File to its containing Object.).freeze,
-      domain: "pcdmmodels:File".freeze,
-      inverseOf: "pcdmmodels:hasFile".freeze,
-      isDefinedBy: "pcdmmodels:".freeze,
+      domain: "pcdm:File".freeze,
+      inverseOf: "pcdm:hasFile".freeze,
+      isDefinedBy: "pcdm:".freeze,
       label: "is file of".freeze,
-      range: "pcdmmodels:Object".freeze,
+      range: "pcdm:Object".freeze,
       subPropertyOf: "ore:isAggregatedBy".freeze,
       type: "rdf:Property".freeze
     property :hasFile,
       comment: %(Links to a File contained by this Object.).freeze,
-      domain: "pcdmmodels:Object".freeze,
-      isDefinedBy: "pcdmmodels:".freeze,
+      domain: "pcdm:Object".freeze,
+      isDefinedBy: "pcdm:".freeze,
       label: "has file".freeze,
-      range: "pcdmmodels:File".freeze,
+      range: "pcdm:File".freeze,
       subPropertyOf: "ore:aggregates".freeze,
       type: "rdf:Property".freeze
     property :hasMember,
@@ -87,7 +87,7 @@ module RDF::Vocab
           not defined as transitive, but applications may treat it as transitive as local needs
           dictate.).freeze,
       domain: "ore:Aggregation".freeze,
-      isDefinedBy: "pcdmmodels:".freeze,
+      isDefinedBy: "pcdm:".freeze,
       label: "has member".freeze,
       range: "ore:Aggregation".freeze,
       subPropertyOf: "ore:aggregates".freeze,
@@ -95,25 +95,25 @@ module RDF::Vocab
     property :hasRelatedObject,
       comment: %(Links to a related Object that is not a component part, such as an object representing a donor agreement or policies that govern the resource.).freeze,
       domain: "ore:Aggregation".freeze,
-      isDefinedBy: "pcdmmodels:".freeze,
+      isDefinedBy: "pcdm:".freeze,
       label: "has related object".freeze,
-      range: "pcdmmodels:Object".freeze,
+      range: "pcdm:Object".freeze,
       subPropertyOf: "ore:aggregates".freeze,
       type: "rdf:Property".freeze
     property :memberOf,
       comment: %(Links from an Object or Collection to a containing Object or Collection.).freeze,
       domain: "ore:Aggregation".freeze,
-      inverseOf: "pcdmmodels:hasMember".freeze,
-      isDefinedBy: "pcdmmodels:".freeze,
+      inverseOf: "pcdm:hasMember".freeze,
+      isDefinedBy: "pcdm:".freeze,
       label: "is member of".freeze,
       range: "ore:Aggregation".freeze,
       subPropertyOf: "ore:isAggregatedBy".freeze,
       type: "rdf:Property".freeze
     property :relatedObjectOf,
       comment: %(Links from an Object to a Object or Collection that it is related to.).freeze,
-      domain: "pcdmmodels:Object".freeze,
-      inverseOf: "pcdmmodels:hasRelatedObject".freeze,
-      isDefinedBy: "pcdmmodels:".freeze,
+      domain: "pcdm:Object".freeze,
+      inverseOf: "pcdm:hasRelatedObject".freeze,
+      isDefinedBy: "pcdm:".freeze,
       label: "is related object of".freeze,
       range: "ore:Aggregation".freeze,
       subPropertyOf: "ore:isAggregatedBy".freeze,
