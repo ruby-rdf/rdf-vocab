7b9f1cb920aebe9efbfea65ca349932a60f51329
Gregg Kellogg <gregg@greggkellogg.net>
2025-07-21 18:27:26 -1000

    Update vocabularies.

--- lib/rdf/vocab/premiseventtype.rb
+++ lib/rdf/vocab/premiseventtype.rb
@@ -9,7 +9,7 @@ module RDF::Vocab
 
     # Ontology definition
     ontology :"http://id.loc.gov/vocabulary/preservation/eventType/",
-      comment: "  Event Types are actions performed within or outside the repository that affect the long term preservation of digital objects.",
+      comment: "                                                       Event Types are actions performed within or outside the repository that affect the long term preservation of digital objects.                              ",
       "http://www.loc.gov/mads/rdf/v1#adminMetadata": [term(
           "http://id.loc.gov/ontologies/RecordInfo#recordChangeDate": "2010-06-01T00:00:01",
           "http://id.loc.gov/ontologies/RecordInfo#recordContentSource": "http://id.loc.gov/vocabulary/preservation/eventType/dlc",
@@ -64,153 +64,203 @@ module RDF::Vocab
     # Extra definitions
     term :acc,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "accession",
+      "http://www.loc.gov/mads/rdf/v1#code": "acc",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :app,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "appraisal",
+      "http://www.loc.gov/mads/rdf/v1#code": "app",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :cap,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "capture",
+      "http://www.loc.gov/mads/rdf/v1#code": "cap",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :com,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "compression",
+      "http://www.loc.gov/mads/rdf/v1#code": "com",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :cop,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "compiling",
-      type: "http://www.loc.gov/mads/rdf/v1#Authority"
+      "http://www.loc.gov/mads/rdf/v1#code": "cop",
+      type: ["http://www.loc.gov/mads/rdf/v1#Authority", "http://www.loc.gov/premis/rdf/v3/Action"]
     term :cre,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "creation",
-      type: "http://www.loc.gov/mads/rdf/v1#Authority"
+      "http://www.loc.gov/mads/rdf/v1#code": "cre",
+      type: ["http://www.loc.gov/mads/rdf/v1#Authority", "http://www.loc.gov/premis/rdf/v3/Action"]
     term :dea,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "deaccession",
+      "http://www.loc.gov/mads/rdf/v1#code": "dea",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :dec,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "decompression",
-      type: "http://www.loc.gov/mads/rdf/v1#Authority"
+      "http://www.loc.gov/mads/rdf/v1#code": "dec",
+      type: ["http://www.loc.gov/mads/rdf/v1#Authority", "http://www.loc.gov/premis/rdf/v3/Action"]
     term :del,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "deletion",
-      type: "http://www.loc.gov/mads/rdf/v1#Authority"
+      "http://www.loc.gov/mads/rdf/v1#code": "del",
+      type: ["http://www.loc.gov/mads/rdf/v1#Authority", "http://www.loc.gov/premis/rdf/v3/Action"]
     term :der,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "decryption",
+      "http://www.loc.gov/mads/rdf/v1#code": "der",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :dig,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "digital signature validation",
-      type: "http://www.loc.gov/mads/rdf/v1#Authority"
+      "http://www.loc.gov/mads/rdf/v1#code": "dig",
+      type: ["http://www.loc.gov/mads/rdf/v1#Authority", "http://www.loc.gov/premis/rdf/v3/Action"]
     term :dis,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "dissemination",
-      type: "http://www.loc.gov/mads/rdf/v1#Authority"
+      "http://www.loc.gov/mads/rdf/v1#code": "dis",
+      type: ["http://www.loc.gov/mads/rdf/v1#Authority", "http://www.loc.gov/premis/rdf/v3/Action"]
     term :dsg,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "digital signature generation",
+      "http://www.loc.gov/mads/rdf/v1#code": "dsg",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :dsp,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "displaying",
-      type: "http://www.loc.gov/mads/rdf/v1#Authority"
+      "http://www.loc.gov/mads/rdf/v1#code": "dsp",
+      type: ["http://www.loc.gov/mads/rdf/v1#Authority", "http://www.loc.gov/premis/rdf/v3/Action"]
     term :enc,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "encryption",
-      type: "http://www.loc.gov/mads/rdf/v1#Authority"
+      "http://www.loc.gov/mads/rdf/v1#code": "enc",
+      type: ["http://www.loc.gov/mads/rdf/v1#Authority", "http://www.loc.gov/premis/rdf/v3/Action"]
     term :exe,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "execution",
-      type: "http://www.loc.gov/mads/rdf/v1#Authority"
+      "http://www.loc.gov/mads/rdf/v1#code": "exe",
+      type: ["http://www.loc.gov/mads/rdf/v1#Authority", "http://www.loc.gov/premis/rdf/v3/Action"]
     term :exp,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "exporting",
-      type: "http://www.loc.gov/mads/rdf/v1#Authority"
+      "http://www.loc.gov/mads/rdf/v1#code": "exp",
+      type: ["http://www.loc.gov/mads/rdf/v1#Authority", "http://www.loc.gov/premis/rdf/v3/Action"]
     term :ext,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "extraction",
+      "http://www.loc.gov/mads/rdf/v1#code": "ext",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :ffa,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "forensic feature analysis",
+      "http://www.loc.gov/mads/rdf/v1#code": "ffa",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :fil,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "filename change",
+      "http://www.loc.gov/mads/rdf/v1#code": "fil",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :fix,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "fixity check",
+      "http://www.loc.gov/mads/rdf/v1#code": "fix",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :for,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "format identification",
-      type: "http://www.loc.gov/mads/rdf/v1#Authority"
+      "http://www.loc.gov/mads/rdf/v1#code": "for",
+      type: ["http://www.loc.gov/mads/rdf/v1#Authority", "http://www.loc.gov/premis/rdf/v3/Action"]
     term :ima,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "imaging",
+      "http://www.loc.gov/mads/rdf/v1#code": "ima",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :ine,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "ingestion end",
+      "http://www.loc.gov/mads/rdf/v1#code": "ine",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :ing,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "ingestion",
+      "http://www.loc.gov/mads/rdf/v1#code": "ing",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :ins,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "ingestion start",
+      "http://www.loc.gov/mads/rdf/v1#code": "ins",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :int,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "interpreting",
-      type: "http://www.loc.gov/mads/rdf/v1#Authority"
+      "http://www.loc.gov/mads/rdf/v1#code": "int",
+      type: ["http://www.loc.gov/mads/rdf/v1#Authority", "http://www.loc.gov/premis/rdf/v3/Action"]
     term :ipc,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "information package creation",
+      "http://www.loc.gov/mads/rdf/v1#code": "ipc",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :ipm,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "information package merging",
+      "http://www.loc.gov/mads/rdf/v1#code": "ipm",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :ips,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "information package splitting",
+      "http://www.loc.gov/mads/rdf/v1#code": "ips",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :mee,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "metadata extraction",
-      type: "http://www.loc.gov/mads/rdf/v1#Authority"
+      "http://www.loc.gov/mads/rdf/v1#code": "mee",
+      type: ["http://www.loc.gov/mads/rdf/v1#Authority", "http://www.loc.gov/premis/rdf/v3/Action"]
     term :mem,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "metadata modification",
-      type: "http://www.loc.gov/mads/rdf/v1#Authority"
+      "http://www.loc.gov/mads/rdf/v1#code": "mem",
+      type: ["http://www.loc.gov/mads/rdf/v1#Authority", "http://www.loc.gov/premis/rdf/v3/Action"]
     term :mes,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "message digest calculation",
+      "http://www.loc.gov/mads/rdf/v1#code": "mes",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :mig,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "migration",
-      type: "http://www.loc.gov/mads/rdf/v1#Authority"
+      "http://www.loc.gov/mads/rdf/v1#code": "mig",
+      type: ["http://www.loc.gov/mads/rdf/v1#Authority", "http://www.loc.gov/premis/rdf/v3/Action"]
     term :mod,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "modification",
-      type: "http://www.loc.gov/mads/rdf/v1#Authority"
+      "http://www.loc.gov/mads/rdf/v1#code": "mod",
+      type: ["http://www.loc.gov/mads/rdf/v1#Authority", "http://www.loc.gov/premis/rdf/v3/Action"]
     term :nor,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "normalization",
+      "http://www.loc.gov/mads/rdf/v1#code": "nor",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :pac,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "packing",
+      "http://www.loc.gov/mads/rdf/v1#code": "pac",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :poa,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "policy assignment",
+      "http://www.loc.gov/mads/rdf/v1#code": "poa",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :prt,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "printing",
-      type: "http://www.loc.gov/mads/rdf/v1#Authority"
+      "http://www.loc.gov/mads/rdf/v1#code": "prt",
+      type: ["http://www.loc.gov/mads/rdf/v1#Authority", "http://www.loc.gov/premis/rdf/v3/Action"]
     term :qua,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "quarantine",
+      "http://www.loc.gov/mads/rdf/v1#code": "qua",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :rec,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "recovery",
+      "http://www.loc.gov/mads/rdf/v1#code": "rec",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :red,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "redaction",
-      type: "http://www.loc.gov/mads/rdf/v1#Authority"
+      "http://www.loc.gov/mads/rdf/v1#code": "red",
+      type: ["http://www.loc.gov/mads/rdf/v1#Authority", "http://www.loc.gov/premis/rdf/v3/Action"]
     term :ref,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "refreshment",
+      "http://www.loc.gov/mads/rdf/v1#code": "ref",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :ren,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "rendering",
-      type: "http://www.loc.gov/mads/rdf/v1#Authority"
+      "http://www.loc.gov/mads/rdf/v1#code": "ren",
+      type: ["http://www.loc.gov/mads/rdf/v1#Authority", "http://www.loc.gov/premis/rdf/v3/Action"]
     term :rep,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "replication",
-      type: "http://www.loc.gov/mads/rdf/v1#Authority"
+      "http://www.loc.gov/mads/rdf/v1#code": "rep",
+      type: ["http://www.loc.gov/mads/rdf/v1#Authority", "http://www.loc.gov/premis/rdf/v3/Action"]
     term :tra,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "transfer",
+      "http://www.loc.gov/mads/rdf/v1#code": "tra",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :unp,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "unpacking",
+      "http://www.loc.gov/mads/rdf/v1#code": "unp",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :unq,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "unquarantine",
+      "http://www.loc.gov/mads/rdf/v1#code": "unq",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
     term :val,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "validation",
-      type: "http://www.loc.gov/mads/rdf/v1#Authority"
+      "http://www.loc.gov/mads/rdf/v1#code": "val",
+      type: ["http://www.loc.gov/mads/rdf/v1#Authority", "http://www.loc.gov/premis/rdf/v3/Action"]
     term :vir,
       "http://www.loc.gov/mads/rdf/v1#authoritativeLabel": "virus check",
+      "http://www.loc.gov/mads/rdf/v1#code": "vir",
       type: "http://www.loc.gov/mads/rdf/v1#Authority"
   end
 end
